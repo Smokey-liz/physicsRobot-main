16b296df2b7eba3f67a3d4f328188c25c26b3682
Smokey-liz <elizahofer02@outlook.com>
2022-06-08 11:31:21 -0400

    SETUP MY GOTOS

--- lilSmokePhysBot.bat
+++ lilSmokePhysBot.bat
@@ -1,9 +1,19 @@
 @ echo off
 echo loading
 pause
-:LOGIN
 set /p USERNAME = "User:"
 set /p PASSWORD = "Password"
 if  %USERNAME% != "Smokey" goto :USER2
 if  %PASSWORD% != "Ee391001771" goto :BADLOGIN
-EXIT /B 0
+goto :GOODLOGIN
+:USER2
+
+
+:BADLOGIN
+echo "restart and try again"
+pause
+return
+
+:GOODLOGIN
+echo "welcome"
+pause
